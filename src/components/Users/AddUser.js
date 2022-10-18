50f8f3d4e24e2e3122e944b24dc8e437ba3ee2b5
Rosie Gardner <rosiegardner78@gmail.com>
2022-10-18 12:43:39 -0700

    import card component from ui folder

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,22 +1,25 @@
 import React from 'react';
+import Card from '../UI/Card';
 
 const AddUser = (props) => {
-  
+
   const addUserHandler = (event) => {
     event.preventDefault();
   }
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">UserName</label>
-      <input 
-        id="username"
-        type="text" />
-      <label htmlFor="age">Age (Years)</label>
-      <input 
-        id="age"
-        type="number" />
-        <button type="submit">Add User</button>
-    </form>
+    <Card>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">UserName</label>
+        <input 
+          id="username"
+          type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input 
+          id="age"
+          type="number" />
+          <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 }
 
