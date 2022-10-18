55c1f9da55b8158038935af1978a6c0221ad0b5e
Rosie Gardner <rosiegardner78@gmail.com>
2022-10-18 15:16:34 -0700

    add state management to handle errors

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -6,7 +6,9 @@ import classes from '../Styles/ErrorModal.module.css';
 const ErrorModal = (props) => {
   return (
     <React.Fragment>
-      <div className={classes.backdrop} />
+      <div className={classes.backdrop}
+        onClick={props.onConfirm} 
+      />
       <Card className={classes.modal}>
         <header className={classes.header}>
           <h2>{props.title}</h2>
@@ -15,7 +17,7 @@ const ErrorModal = (props) => {
           <p>{props.message}</p>
         </div>
         <footer className={classes.actions}>
-          <Button>BYE</Button>
+          <Button onClick={props.onConfirm}>BYE</Button>
         </footer>
       </Card>
     </React.Fragment>
