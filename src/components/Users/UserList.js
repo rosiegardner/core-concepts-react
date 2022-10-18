dd775f4e265426d58820a3edd176b5f23e637246
Rosie Gardner <rosiegardner78@gmail.com>
2022-10-18 14:52:36 -0700

    add key id to userList

--- src/components/Users/UserList.js
+++ src/components/Users/UserList.js
@@ -7,11 +7,12 @@ const UserList = (props) => {
   return (
     <Card className={classes.users}>
       <ul>
-        {props.users.map(user => 
-          <li>
-            {user.name} ({user.age} years old)
+        {props.users.map((user) => (
+          <li key={user.id}>
+            {user.name} 
+            ({user.age} years old)
           </li>
-        )}
+        ))}
       </ul>
     </Card>
   )
