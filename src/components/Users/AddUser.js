c5962ff6d5e8c947c037559e5e8059b41d1165ed
Rosie Gardner <rosiegardner78@gmail.com>
2022-10-18 12:34:47 -0700

    add user input form

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
-const AddUser =  () => {
+const AddUser = (props) => {
+  
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  }
   return (
-    <h2> welcome </h2>
+    <form onSubmit={addUserHandler}>
+      <label htmlFor="username">UserName</label>
+      <input 
+        id="username"
+        type="text" />
+      <label htmlFor="age">Age (Years)</label>
+      <input 
+        id="age"
+        type="number" />
+        <button type="submit">Add User</button>
+    </form>
   );
 }
 
